3adf36aee43b7fe4ac547c3081e06d57feeef3c3
Derrick Otte <derrickotte@gmail.com>
2020-05-05 20:52:43 -0400

    Extract version history out to a module
    - Functionality is the exact same
    - Add additional check to test
    - Remove code from model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,7 @@
 class Product < ApplicationRecord
   include VersionHistory
+
   has_many :product_versions, dependent: :destroy
-  after_commit :send_changes_to_version_history, if: :persisted?
 
-  private
-
-  def send_changes_to_version_history
-    new_version = Hash.new
-
-    self.previous_changes.each do |key, changes|
-      new_version[key] = changes.last
-    end
-
-    self.product_versions.create!(new_version)
-  end
+  after_commit -> { send_changes_to_version_history(self) }, if: :persisted?
 end
--- lib/version_history.rb
+++ lib/version_history.rb
@@ -1,5 +1,12 @@
 module VersionHistory
-  def print_out_string
-    puts "Hello" 
+  def send_changes_to_version_history(persisted_object)
+    version_history_class = "#{persisted_object.class.name.downcase}_versions"
+    new_version = Hash.new
+
+    persisted_object.previous_changes.each do |key, changes|
+      new_version[key] = changes.last
+    end
+
+    persisted_object.send(version_history_class).create!(new_version)
   end
 end
